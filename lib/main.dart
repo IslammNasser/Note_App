886533921e20c02cf367e97ce58c47bb8177d200
IslammNasser <elamnasser820@gmail.com>
2023-12-28 13:49:40 +0200

    init hive and open note box

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/views/notes_view.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: NoteApp(),
-  ));
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes');
+  runApp(
+    NoteApp(),
+  );
 }
 
 class NoteApp extends StatelessWidget {
@@ -11,6 +15,9 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotsView(),
+    );
   }
 }
